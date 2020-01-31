d9b967fd674f96eb07ee1a80f699e0116d09b29f
Priya Sajid <priyasajid@hotmail.com>
2020-01-31 12:43:39 +0100

    adding cern username to user

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -6,6 +6,12 @@ if os.environ['USER'] in ['phussain']:
     plot_directory                      = "/afs/hephy.at/user/p/phussain/www/stopsCompressed/"
     private_results_directory           = "/afs/hephy.at/data/cms02/"
     
+if os.environ['USER'] in ['prhussai']:
+    results_directory                   = "/afs/hephy.at/data/cms02/StopsCompressed/"
+    postProcessing_output_directory     = "/afs/hephy.at/data/cms02/StopsCompressed/nanoTuples/"
+    plot_directory                      = "/afs/hephy.at/user/p/phussain/www/stopsCompressed/"
+    private_results_directory           = "/afs/hephy.at/data/cms02/"
+
 if os.environ['USER'] in ['rschoefbeck']:
     results_directory                   = "/afs/hephy.at/data/cms02/StopsCompressed/"
     postProcessing_output_directory     = "/afs/hephy.at/data/cms02/StopsCompressed/nanoTuples/"
