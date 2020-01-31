950219c85ef685118af79c86e5a0911acb65e44b
Priya Sajid <priyasajid@hotmail.com>
2020-01-31 12:45:04 +0100

    users updated

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -7,10 +7,10 @@ if os.environ['USER'] in ['phussain']:
     private_results_directory           = "/afs/hephy.at/data/cms02/"
     
 if os.environ['USER'] in ['prhussai']:
-    results_directory                   = "/afs/hephy.at/data/cms02/StopsCompressed/"
-    postProcessing_output_directory     = "/afs/hephy.at/data/cms02/StopsCompressed/nanoTuples/"
-    plot_directory                      = "/afs/hephy.at/user/p/phussain/www/stopsCompressed/"
-    private_results_directory           = "/afs/hephy.at/data/cms02/"
+    results_directory                   = "/afs/cern.ch/work/p/prhussai/private/StopsCompressed"
+    postProcessing_output_directory     = "/afs/cern.ch/work/p/prhussai/private/StopsCompressed/nanoTuples/"
+    plot_directory                      = "/eos/user/p/prhussai/www/"
+    private_results_directory           = "/afs/cern.ch/work/p/prhussai/private/StopsCompressed/results"
 
 if os.environ['USER'] in ['rschoefbeck']:
     results_directory                   = "/afs/hephy.at/data/cms02/StopsCompressed/"
