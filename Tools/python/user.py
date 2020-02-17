7f232111627abda0293d6ee4d72293f6fd20e486
priya sajid <priyasajid@hotmail.com>
2020-02-17 11:17:15 +0100

    dding clip username&details

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -6,6 +6,12 @@ if os.environ['USER'] in ['phussain']:
     plot_directory                      = "/afs/hephy.at/user/p/phussain/www/stopsCompressed/"
     private_results_directory           = "/afs/hephy.at/data/cms02/"
     
+if os.environ['USER'] in ['priya.hussain']:
+    results_directory                   = "/mnt/hephy/cms/priya.hussain/StopsCompressed/"
+    postProcessing_output_directory     = "/mnt/hephy/cms/priya.hussain/StopsCompressed/nanoTuples/"
+    plot_directory                      = "/users/priya.hussain/www/StopsCompressed/"
+    private_results_directory           = "/mnt/hephy/cms/priya.hussain/"
+
 if os.environ['USER'] in ['rschoefbeck']:
     results_directory                   = "/afs/hephy.at/data/cms02/StopsCompressed/"
     postProcessing_output_directory     = "/afs/hephy.at/data/cms02/StopsCompressed/nanoTuples/"
