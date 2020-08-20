04aa09ab728e127b6fb3b2a1c9b38578958d7053
priya sajid <priyasajid@hotmail.com>
2020-08-20 16:53:11 +0200

    minor

--- samples/python/default_locations.py
+++ samples/python/default_locations.py
@@ -1,10 +1,27 @@
 class default_locations:
 
-    mc_2016_data_directory = "/mnt/hephy/cms/priya.hussain/StopsCompressed/nanoTuples/"
-    mc_2016_postProcessing_directory = "compstops_2016_nano_v3/MetSingleLep/"
+    #mc_2016_data_directory = "/mnt/hephy/cms/priya.hussain/StopsCompressed/nanoTuples/"
+    #change it to scratch for v6 of 2016:
+    mc_2016_data_directory = "/scratch/priya.hussain/StopsCompressed/nanoTuples/"
+    #mc_2016_postProcessing_directory = "compstops_2016_nano_v8/MetSingleLep/"
+    mc_2016_postProcessing_directory = "compstops_2016_nano_v15/MetSingleLep/"
+    signal_2016_postProcessing_directory = "compstops_2016_nano_v20/MetSingleLep/"
 
-    data_2016_data_directory = "/mnt/hephy/cms/priya.hussain/StopsCompressed/nanoTuples/"
-    data_2016_postProcessing_directory = "compstops_2016_nano_v4/MetSingleLep/"
+    
+    #data_2016_data_directory = "/mnt/hephy/cms/priya.hussain/StopsCompressed/nanoTuples/"
+    data_2016_data_directory = "/scratch/priya.hussain/StopsCompressed/nanoTuples/"
+    data_2016_postProcessing_directory = "compstops_2016_nano_v18/MetSingleLep/"
+    #data_2016_postProcessing_directory = "compstops_2016_nano_v15/MetSingleLep/"
+    #data_2016_postProcessing_directory = "compstops_2016_nano_v8/MetSingleLep/"
 
-    mc_2018_data_directory = "/afs/hephy.at/data/cms02/StopsCompressed/nanoTuples/"
-    mc_2018_postProcessing_directory = "compstops_2018_nano_v1/singleLep/"
+    mc_2017_data_directory = "/mnt/hephy/cms/priya.hussain/StopsCompressed/nanoTuples/"
+    mc_2017_postProcessing_directory = "compstops_2017_nano_v5/MetSingleLep/"
+
+    data_2017_data_directory = "/mnt/hephy/cms/priya.hussain/StopsCompressed/nanoTuples/"
+    data_2017_postProcessing_directory = "compstops_2017_nano_v5/MetSingleLep/"
+
+    mc_2018_data_directory = "/scratch/priya.hussain/StopsCompressed/nanoTuples/"
+    mc_2018_postProcessing_directory = "compstops_2018_nano_v6/MetSingleLep/"
+
+    data_2018_data_directory = "/scratch/priya.hussain/StopsCompressed/nanoTuples/"
+    data_2018_postProcessing_directory = "compstops_2018_nano_v6/MetSingleLep/"
