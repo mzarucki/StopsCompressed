bce6035c0303c3c529e7c7ccfc24fc9105f603be
janik.andrejkovic <janik.andrejkovic@clip-c1-1.cbe.vbc.ac.at>
2020-10-21 11:51:46 +0200

    adding user Janik

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -13,6 +13,12 @@ if os.environ['USER'] in ['priya.hussain']:
     postProcessing_output_directory     = "/mnt/hephy/cms/priya.hussain/StopsCompressed/nanoTuples/"
     plot_directory                      = "/mnt/hephy/cms/priya.hussain/www/StopsCompressed/"
     private_results_directory           = "/mnt/hephy/cms/priya.hussain/"
+if os.environ['USER'] in ['janik.andrejkovic']:
+    results_directory                   = "/mnt/hephy/cms/janik.andrejkovic/StopsCompressed/"
+    cache_directory                     = "/mnt/hephy/cms/janik.andrejkovic/StopsCompressed/"
+    postProcessing_output_directory     = "/mnt/hephy/cms/janik.andrejkovic/StopsCompressed/nanoTuples/"
+    plot_directory                      = "/mnt/hephy/cms/janik.andrejkovic/www/StopsCompressed/"
+    private_results_directory           = "/mnt/hephy/cms/janik.andrejkovic/"
 if os.environ['USER'] in ['prhussai']:
     results_directory                   = "/afs/cern.ch/work/p/prhussai/private/StopsCompressed"
     postProcessing_output_directory     = "/afs/cern.ch/work/p/prhussai/private/StopsCompressed/nanoTuples/"
