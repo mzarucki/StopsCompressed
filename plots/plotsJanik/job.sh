8db0c99a33facebdbfd02823e860e95d64613de3
janik.andrejkovic <janik.andrejkovic@clip-login-1.cbe.vbc.ac.at>
2021-04-30 15:08:43 +0200

    adding FR plots

---
+++ plots/plotsJanik/job.sh
@@ -0,0 +1,4 @@
+python analysisPlots.py --era Run2016 --reweightPU Central --targetDir testing --selection nISRJets1p-ntau0-lepSel-deltaPhiJetsInverted-jet3Veto-met200-ht300
+
+
+python analysisPlots.py --era Run2016 --reweightPU Central --targetDir closure_testing_v0 --selection nISRJets1p-ntau0-lepSel-deltaPhiJetsInverted-jet3Veto-met200-ht300
