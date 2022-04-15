9bdf6aaf98fe10f9ac7be8d4ccc740f67d99dd6d
Dietrich Liko <Dietrich.Liko@oeaw.ac.at>
2022-04-15 13:35:55 +0200

    Scripts to check run_estimate

--- plots/plotsPriya/sub_analysisPlots.sh
+++ plots/plotsPriya/sub_analysisPlots.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-#for era in "Run2016" "Run2016preVFP" "Run2016postVFP" "Run2017" "Run2018"
-for era in "Run2016" "Run2017" "Run2018"
+for era in "Run2016" "Run2016preVFP" "Run2016postVFP" "Run2017" "Run2018"
 do
     sbatch --job-name="plot_$era" \
 	   --output="$SCRATCHDIR/batch_output/analyis_plot_$era-%A.out" \
